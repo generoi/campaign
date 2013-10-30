b302f86d95494f3cec95db546d361ebfee347dee
Oskar Schöldström <public@oxy.fi>
2013-10-30 09:15:09 +0200

    Implement campaign_banner features

---
+++ campaign_banner/banner.php
@@ -0,0 +1,61 @@
+<?php
+
+// @TODO can we figure out correct path without bootstrapping drupal?
+define('DRUPAL_ROOT', realpath(__DIR__ . '/../../../../../..'));
+
+// Read $databases from drupal settings.
+require_once DRUPAL_ROOT . '/sites/default/settings.php';
+// Read CAMPAIGN_BANNER_UPLOAD_DIR
+require_once __DIR__ . '/campaign_banner.module';
+
+class DB {
+  public $db;
+
+  protected $table = "campaign_counter";
+
+  function __construct($obj) {
+    $user = $obj['default']['default']['username'];
+    $pass = $obj['default']['default']['password'];
+    $host = $obj['default']['default']['host'];
+    $path = $obj['default']['default']['database'];
+
+    $this->db = new PDO("mysql:host=$host;dbname=$path", $user, $pass);
+  }
+
+  function increment($ref) {
+    $sth = $this->db->prepare("INSERT INTO $this->table (hits, ref) VALUES (1, :ref) ON DUPLICATE KEY UPDATE hits = hits + 1");
+    return $sth->execute(array(':ref' => $ref));
+  }
+
+  function count() {
+    return $this->db->query("SELECT COUNT(*) FROM $this->table")->fetchColumn();
+  }
+}
+
+$image = isset($_GET['image']) ? $_GET['image'] : NULL;
+$campaign = isset($_GET['campaign']) ? $_GET['image'] : NULL;
+$ref = isset($_GET['ref']) ? $_GET['ref'] : NULL;
+
+if (!isset($image) || !isset($campaign)) {
+  exit();
+}
+// The relative path to the uploads directory
+$upload_dir = str_replace('public://', DRUPAL_ROOT . '/sites/default/files/', CAMPAIGN_BANNER_UPLOAD_DIR) . $campaign;
+$extension = pathinfo($image, PATHINFO_EXTENSION);
+$image = $upload_dir . '/' . $image;
+$mimetypes = array(
+  'png' => 'image/png',
+  'jpg' => 'image/jpeg',
+  'gif' => 'image/gif',
+);
+
+if (isset($ref)) {
+  $db = new DB($databases);
+  $db->increment($ref);
+}
+
+header('Content-Type: ' . $mimetypes[$extension]);
+header('Content-Length: ' . filesize($image)); // optimization
+$f = fopen($image, 'rb');
+fpassthru($f);
+fclose($f);
---
+++ campaign_banner/campaign_banner.js
@@ -0,0 +1,69 @@
+(function($) {
+  Drupal.behaviors.campaignBanner = {
+    attach: function (context) {
+      $('.banner-generator-wrapper').once(function () {
+        Drupal.campaignBanner.attachListeners(this, $(this).data());
+      });
+    }
+  };
+
+  Drupal.campaignBanner = Drupal.campaignBanner || {};
+
+  Drupal.campaignBanner.attachListeners = function(el, data) {
+    var $el = $(el),
+      $output = $el.find('textarea').bind('focus', forceSelect);
+
+    $el.on('click', '.banner-generator', data, function(event) {
+      Drupal.campaignBanner.generateBanner.call(this, event, $output);
+    });
+  }
+
+  Drupal.campaignBanner.generateBanner = function(event, $output) {
+    var ref = clean(window.prompt(Drupal.t('Enter the URL of your blog'), 'http://')),
+      filename = $(this).find('img').prop('src'),
+      image = filename + '&ref=' + window.encodeURIComponent(ref),
+      href = event.data.href,
+      alt = event.data.alt,
+      snippet = Drupal.campaignBanner.generate(href, image, alt);
+
+    if (!ref) snippet = Drupal.t('Invalid URL.');
+    $output.val(snippet).select();
+    $('html, body').animate({
+      scrollTop: $output.offset().top
+    }, 500);
+    event.preventDefault();
+  }
+
+  Drupal.campaignBanner.generate = function (href, image, alt) {
+    return [
+      '<a href="' + href + '">',
+        '<img src="' + image + '" alt="' + alt + '" />',
+      '</a>'
+    ].join('');
+  };
+
+  function forceSelect() {
+    var $this = $(this);
+    $this.select();
+    $this.mouseup(function() {
+      $this.unbind('mouseup');
+      return false;
+    });
+  }
+
+
+  function clean(url) {
+    var match = url.match(/^(?:https?:\/\/)?((?:\w+\.){1,2}\w+(?:\/\w+(?!\.))?).*/);
+    if (match) return match[1];
+    return false;
+  }
+  // Test regexp
+  // var regexp = /^(?:https?:\/\/)?((?:\w+\.){1,2}\w+(?:\/\w+(?!.))?).*/;
+  // ['http://www.google.com'
+  //   ,'https://www.google.com'
+  //   ,'http://google.com'
+  //   ,'http://www.google.com/path'
+  //   ,'http://www.google.com/path.php'
+  //   ,'www.google.com/path.php'
+  // ].forEach(function(url) { console.log(url.match(regexp)); });
+}(jQuery));
